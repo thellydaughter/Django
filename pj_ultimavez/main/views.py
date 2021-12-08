0fd6e11670c46d50d9afc10f6ba753bdbaafa9f2
unknown <thellymyteen@gmail.com>
2021-12-08 08:45:33 -0300

    Meu comentário bonito

---
+++ pj_ultimavez/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def sobre(request):
+    return render(request, 'main/sobre.html')
+
+
+def contato(request):
+    return render(request, 'main/contato.html')
+
+
